4918ba58babef23a11ec3e3c1f08c4cad9059bf7
Your Name <innovazania@gmail.com>
2022-11-22 18:27:17 +0200

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -802,13 +802,13 @@
                       {{-- <a href="/report/term-based/" class="nav-link"> --}}
                         {{-- /report/term-based/class --}}
                       <li class="nav-item">
-                        <a href="/reports/parent/term-based/" class="nav-link">
+                        <a href="/report/term-based/" class="nav-link">
                           <i class="nav-icon fas fa-chevron-circle-right"></i>
                           <p>Stream Report</p>
                         </a>
                       </li>
                       <li class="nav-item">
-                        <a href="/reports/parent/term-based/class" class="nav-link">
+                        <a href="/report/term-based/class" class="nav-link">
                           <i class="nav-icon fas fa-chevron-circle-right"></i>
                           <p>Class Report</p>
                         </a>
