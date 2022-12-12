417e5258f009a18c837b0bb7dfd8e71e53bb9075
Your Name <innovazania@gmail.com>
2022-12-12 12:56:10 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -171,7 +171,7 @@ Route::get('view/terms/{id}', [TermController::class,'index'])->name('term.index
 Route::post('terms/add/', [TermController::class,'store'])->name('term.store');
 Route::post('terms/edit/', [TermController::class,'edit'])->name('term.edit');
 Route::post('terms/update', [TermController::class,'update'])->name('term.update');
-Route::delete('/terms/delete/', [TermController::class,'destroy'])->name('term.destroy');
+Route::post('/terms/delete/', [TermController::class,'destroy'])->name('term.destroy');
 
 //Terms
 // Route::get('/academic-admin/terms', [ShunifuTermController::class,'create'])->name('term.create');
