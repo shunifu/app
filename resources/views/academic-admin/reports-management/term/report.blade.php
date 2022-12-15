41e50e2b30f2f0e8e6aa34c1e4ba1188da6fc73c
Your Name <innovazania@gmail.com>
2022-12-15 20:34:57 +0200

    update

--- resources/views/academic-admin/reports-management/term/report.blade.php
+++ resources/views/academic-admin/reports-management/term/report.blade.php
@@ -1192,7 +1192,7 @@ School Stamp
                           <div class="text-center">
                           @if ($variable->principal_signature==1)
                          
-                            <img class="img-fluid " width="80" height="80" src="{{$school_is->base64}} " alt="">
+                            <img class="img-fluid " width="120" height="120" src="{{$school_is->base64}} " alt="">
                                @else       
                                <img class="img-fluid " width="120" height="120" src="https://res.cloudinary.com/innovazaniacloud/image/upload/v1667299468/image_sig_kmjh1n.jpg" alt="">            
                           @endif
