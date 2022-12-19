81422c0a22182e413aa56d35f18d42d21e37c03a
Your Name <innovazania@gmail.com>
2022-12-19 17:24:03 +0200

    position

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -1729,8 +1729,7 @@ if ($request->indicator=="scoresheet" OR $request->indicator=="manual_promotion"
        MAX(CASE WHEN subjects.subject_code=122 THEN student_subject_averages.student_average END) AS 'Consumer',
        MAX(CASE WHEN subjects.subject_code=121 THEN student_subject_averages.student_average END) AS 'Entreprenuership',
        MAX(CASE WHEN subjects.subject_code=123 THEN student_subject_averages.student_average END) AS 'Agriculture',
-       MAX(CASE WHEN subjects.subject_code=123 THEN student_subject_averages.student_average END) AS 'Agriculture',
-       MAX(CASE WHEN subjects.subject_code=150 THEN student_subject_averages.student_average END) AS 'French'
+       MAX(CASE WHEN subjects.subject_code=123 THEN student_subject_averages.student_average END) AS 'Agriculture'
         FROM student_subject_averages INNER JOIN teaching_loads ON teaching_loads.id=student_subject_averages.teaching_load_id
         INNER JOIN subjects ON teaching_loads.subject_id=subjects.id
         INNER JOIN users ON users.id=student_subject_averages.student_id
