0c147efd1c44e7f928908259301309f5bf4b61af
Your Name <innovazania@gmail.com>
2022-12-28 12:06:36 +0200

    upfa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
          {{-- <link href="/css/app.css" rel="stylesheet"> --}}
       
          @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
    
     
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
+
 import laravel from 'laravel-vite-plugin'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
+    manifest: true,
     plugins: [
         laravel([
             'resources/sass/app.scss',
             'resources/js/app.js',
         ]),
     ],
+    server: {
+        https: false,
+        host: env('APP_URL'),
+    },
 });
+
+
