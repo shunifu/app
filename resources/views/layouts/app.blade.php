81638f869080d28219d66938913c0cc179b16400
Your Name <innovazania@gmail.com>
2022-12-28 12:16:12 +0200

    upfa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
          {{-- <link  href="/css/app.css" rel="stylesheet"> --}}
          
          {{-- <link href="/css/app.css" rel="stylesheet"> --}}
+
+         {{ @vite('resources/js/app.js') }}
       
          @vite(['resources/sass/app.scss', 'resources/js/app.js'])
          {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
