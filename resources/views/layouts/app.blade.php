e29127ce52e01847f35fcb51221cb16ed2a060fa
Your Name <innovazania@gmail.com>
2023-01-16 12:10:53 +0200

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -670,16 +670,16 @@
                     </li>
 
 
-                    {{-- <li class="nav-item has-treeview">
+                     <li class="nav-item has-treeview">
                       <a href="#" class="nav-link">
                         <i class="fas fa-chart-bar nav-icon"></i>
                         <p>
                          Term Insights 
                           <i class="fas fa-angle-left right"></i>
                         </p>
-                      </a> --}}
+                      </a> 
   
-                      {{-- <ul class="nav nav-treeview">
+                       <ul class="nav nav-treeview">
                         <li class="nav-item">
                           <a href="/analytics/term-based" class="nav-link">
                             <i class="nav-icon fas fa-check-circle"></i>
@@ -696,7 +696,7 @@
                           </a>
                         </li> 
   
-                      </ul> --}}
+                      </ul> 
                       </li>
 
                   
