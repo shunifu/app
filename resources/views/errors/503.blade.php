6cf4b4bebaea0e041070d9a9d0c9aae8c037b512
Your Name <innovazania@gmail.com>
2023-04-25 17:39:55 +0200

    down

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('System Ugrade'))
+@section('title', __('System Upgrade'))
 @section('code', '503')
 @section('message', __('Shunifu is currently undergoing an upgrade. Please try again later. Siyabonga'))
