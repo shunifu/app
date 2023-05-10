b2ed0eeecdf2c8472f85e085d204932e1605789e
Your Name <innovazania@gmail.com>
2023-05-10 17:20:06 +0200

    new

--- resources/views/analytics/scoresheet/index.blade.php
+++ resources/views/analytics/scoresheet/index.blade.php
@@ -630,11 +630,7 @@ where student_id = ".$student->learner_id.""));
                         <span class="text-danger">{{ $student->Computer}}% </span>
                         @endif
                     </td>
-<<<<<<< HEAD
-                 
-=======
-                  
->>>>>>> d5b31487 (updaded)
+
                     @endforeach
                   
 
