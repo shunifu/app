417998340bbe1413c2e1da7160b060e53a182169
Your Name <innovazania@gmail.com>
2023-08-05 13:57:11 +0200

    update

--- resources/views/academic-admin/reports-management/term/report.blade.php
+++ resources/views/academic-admin/reports-management/term/report.blade.php
@@ -1439,7 +1439,7 @@ echo '<span class="font-italic font-weight-light">'.substr($key_t->name, 0, 1).'
                          
                             <img class="img-fluid " width="80" height="80" src="{{$school_is->base64}} " alt="">
                                @else       
-                               <img class="img-fluid " width="120" height="120" src="https://res.cloudinary.com/innovazaniacloud/image/upload/v1667299468/image_sig_kmjh1n.jpg" alt="">            
+                               <img class="img-fluid " width="220" height="220" src="https://res.cloudinary.com/innovazaniacloud/image/upload/v1667299468/image_sig_kmjh1n.jpg" alt="">            
                           @endif
                          
                         </div>
