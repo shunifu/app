5ab4809812d6b5c01691100765c49b26f8130295
Your Name <innovazania@gmail.com>
2023-08-12 12:39:57 +0200

    update

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('System Upgrade'))
 @section('code', '503')
-@section('message', __('System is currently undergoing an upgrade.'))
+@section('message', __('Shunifu  is currently undergoing a CBE Upgrade. Please check again later'))
