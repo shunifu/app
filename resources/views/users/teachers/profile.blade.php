9427276c1deb151cec07bbfb7da268f6a998b310
Your Name <innovazania@gmail.com>
2023-08-25 12:39:32 +0200

    update

--- resources/views/users/teachers/profile.blade.php
+++ resources/views/users/teachers/profile.blade.php
@@ -120,7 +120,7 @@
         @else
             <div class="col-md-12 mx-auto">  
         @endif
-<small>Please use this section after you have updated your password. </small>
+{{-- <small>Please use this section after you have updated your password. </small> --}}
       
             <div class="card card-light  ">
                 <div class="card-body">
