e8df2d2826ec91620b27321fcdc9d48aa55cd583
Your Name <innovazania@gmail.com>
2023-10-22 12:12:41 +0200

    update

---
+++ app/Http/Controllers/StudenFeeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudenFee;
+use Illuminate\Http\Request;
+
+class StudenFeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StudenFee  $studenFee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StudenFee $studenFee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\StudenFee  $studenFee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StudenFee $studenFee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\StudenFee  $studenFee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StudenFee $studenFee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StudenFee  $studenFee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StudenFee $studenFee)
+    {
+        //
+    }
+}
---
+++ app/Models/StudenFee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudenFee extends Model
+{
+    use HasFactory;
+}
