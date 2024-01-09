8785550edde692a13b648ba71f1f88544d86f383
Your Name <innovazania@gmail.com>
2024-01-09 22:23:01 +0200

    update bulk

--- resources/views/users/students/registration-pathways/bulk.blade.php
+++ resources/views/users/students/registration-pathways/bulk.blade.php
@@ -1,158 +1,220 @@
 <x-app-layout>
     <x-slot name="header">
-      
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
     </x-slot>
-    <div class="card card-light">
+    <div class="card card-light  ">
       <div class="card-header">
         <h3 class="card-title">Student Registration Portal</h3>
       </div>
-  
-        <img class="card-img-top"  src="https://res.cloudinary.com/innovazaniacloud/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,l_text:montserrat_32_style_light_align_center:Multiple-Student%20Registration Form,w_0.4,y_0.18/v1650135733/pexels-rodnae-productions-10375992_1_shjypq.jpg">
+
+        <img class="card-img-top"  src="https://res.cloudinary.com/innovazaniacloud/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,l_text:montserrat_32_style_light_align_center:Single-Student%20Registration Form,w_0.4,y_0.18/v1650135733/pexels-rodnae-productions-10375992_1_shjypq.jpg">
         <div class="card-body">
           <h4 class="lead">Student Registration Portal</h4>
-         
+
           <hr>
          <div class="text-muted">
-          Hi, <span class="text-bold">{{Auth::user()->salutation}} {{Auth::user()->lastname}}</span>. Welcome to the  bulk-student-add pathway. This is where you add a multiple-students at a time. Below is a form where you will add the needed student details. <br>
-          <span class="text-danger">Please note that the parent email and parent cell are optional.</span><br>
-          To go back click <a href="/users/student">here</a> 
+          Hi, <span class="text-bold">{{Auth::user()->salutation}} {{Auth::user()->lastname}}</span>. Welcome to the  bulk-student registration pathway. This is where you add multiple students at a time. Below is a form where you will add the needed student details. <br>
+          To go back click <a href="/users/student">here</a>
          </div>
-       
-        </div>
-    </div> 
 
-    <div class="card text-left">
-            
-      <div class="card-body">
-    <form action="{{route('pathway.bulk_store')}}" method="post">  
-      @csrf
-    <div class="form-row" id="master_div">
-      <div class="col-md-1 form-group">
-
-                    <x-jet-label> Class</x-jet-label>
-                    <select class="form-control" name="grade[]">
-                    <option value=""> Class</option>
-                    @foreach ($class as $student_class)
-                    <option value={{$student_class->id}}>{{$student_class->grade_name}}</option>
-                    @endforeach
-                    </select>
-                    @error('grade')
-                    <span class="text-danger">{{$message}}</span>  
-                    @enderror
         </div>
-          
-    
-        <div class="col-md-2 form-group">
-              <div class="col form-group">
-                  <x-jet-label>Surname</x-jet-label>
-                  <input type="text" name="lastname[]" id="lastname" required    class="form-control" placeholder="Student Surname" >
-                  @error('lastname')
-                  <span class="text-danger">{{$message}}</span>  
-                  @enderror
-                  </div></p>
-        </div>
-
-        <div class="col-md-2 form-group">
-          <div class="col form-group">
-              <x-jet-label>Name</x-jet-label>
-              <input type="text" name="name[]" id="name" required    class="form-control" placeholder="Student Name" >
-              @error('name')
-              <span class="text-danger">{{$message}}</span>  
-              @enderror
-              </div></p>
     </div>
 
-    <div class="col-md-2 form-group">
-      <div class="col form-group">
-          <x-jet-label>Middlename</x-jet-label>
-          <input type="text" name="middlename[]" id="name"     class="form-control" placeholder="Student Middlename" >
-          @error('nmiddleame')
-          <span class="text-danger">{{$message}}</span>  
-          @enderror
-          </div></p>
-    </div>
-
-    <div class="col-md-2 form-group">
-      <div class="col form-group">
-        <x-jet-label>Gender</x-jet-label>
-    <select class="form-control" name="gender[]" required id="gender">
-      <option value="">Select Gender</option>
-      <option value="male">Male</option>
-      <option value="female">Female</option>
-      </select>
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Bulk Registration Pathway</h3>
       </div>
+    <div class="card-body">
+      <div class="row">
+
+
+
+
+                             <!-- form start -->
+              <form action="{{route('pathway.bulk_store')}}" method="post">
+
+                      @csrf
+<div class="row">
+
+    <div class="col" >
+
+        <div class="form-group">
+            <label> Student Class <span style="color: red">*</span></label>
+            <select class="form-control" name="student_class[]">
+            <option value=""> Select Class</option>
+
+            @foreach ($classes as $class)
+            <option value="{{$class->id}}">{{$class->grade_name}}</option>
+            @endforeach
+
+            </select>
+            @error('student_class')
+            <span class="text-danger">{{$message}}</span>
+            @enderror
+           </div>
+    </div>
+
+    <div class="col" >
+
+        <div class="pb-4 mb-2"></div>
+
+
+<button class="btn btn-success"  type="button" id="add_more"><i class="fa fa-circle"></i> Add More Students</button>
+
+
     </div>
 
 
+</div>
+<hr>
+                      <div class="form-row" id="master-div">
 
 
-      </div>
+
+                        <div class="col-12 col-sm-6 col-md-2">
+                          <div class="form-group">
+                        <label> Name</label>
+    <x-jet-input name="first_name[]" required value="{{ old('first_name') }}" placeholder="First Name" ></x-jet-input>
+                        @error('first_name')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                       </div>
+
+                        </div>
 
 
-<div id="slave_div">
+                        <div class="col-12 col-sm-6 col-md-2">
+                          <div class="form-group">
+                        <label>Middle Name</label>
+                        <x-jet-input name="middle_name[]" value="{{ old('middle_name') }}" placeholder="Middle Name" ></x-jet-input>
+                        @error('middle_name')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                       </div>
+                        </div>
 
-  <div class="col form-group">
-    <x-jet-label class="label"> Add More Students</x-jet-label>
-      <button type="button" class="btn btn-success" name="add"
-      id="add_input" type="button"><i class="fas fa-plus-circle"></i></button>
-     
-      </div>
+                        <div class="col-12 col-sm-6 col-md-2">
+                          <div class="form-group">
+                        <label>Last Name</label>
+                        <x-jet-input name="last_name[]" required value="{{ old('last_name') }}" placeholder="Last Name" ></x-jet-input>
+                        @error('last_name')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                        </div>
+                        </div>
+
+
+
+                            <div class="col-12 col-sm-6 col-md-2">
+                                <label> Status <span style="color: red">*</span></label></label>
+                                <select class="form-control" name="student_status[]">
+                                <option value=""> Status</option>
+                                <option value="ovc">OVC</option>
+                                <option value="non_ovc">NON-OVC</option>
+                                </select>
+                                @error('student_status')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                               </div>
+
+                               <div class="col-12 col-sm-6 col-md-2">
+                                <label> Sponsor <span style="color: red">*</span></label></label>
+                                <select class="form-control" name="student_sponsor[]">
+                                <option value=""> Sponsor</option>
+
+                                </select>
+                                @error('student_sponsor')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+
+
+
+                               </div>
+
+                               <div class="col-12 col-sm-6 col-md-2">
+                                <label class="pb-2" for=""></label>
+
+                                <div class="remove-btn"></div>
+
+
+
+                               </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </div>
 
-      {{-- <div id="slave_div">
-        <div class="col-md-1 form-group">
-          <div class="col form-group">
-            <x-jet-label class="label"> Add More</x-jet-label>
-              <input type="button" class="btn btn-danger" name="add"
-              id="add_input" type="button"><i class="fas fa-plus-circle"></i></button>
-             
+
+
+              <!-- /.end of form row -->
+
+                  <div  id="slave-div">
+
+                  </div>
+
+              <div class="card-footer">
+                <x-jet-button>Register Student</x-jet-button>
               </div>
-        </div>
-      </div> --}}
-
-        <x-jet-button>Register Student</x-jet-button>
-     
-    </form>
-      </div>
-    </div>
-          
- 
+            </form>
 
 
-          <script>
-              $(document).ready(function () {
-                
-                  $("#add_input").click(function (e) { 
+              </div>
+              {{-- end of row --}}
 
-             //       $("#master_div").clone().appendTo("#slave_div").val;
-             .clone().appendTo('.fieldset-clone').find('.name').val('');
-               
-                    var copySlot = $("#master_div").clone(true).val(" "); // clone here 
-                    $('label', copySlot).hide(); // hide Clone Label
-                    $('button', copySlot).hide(); // hide Clone Label
-                    $('text', copySlot).val(""); // hide Clone Label
-                    copySlot.appendTo("#slave_div"); // append now
-                     
-                //   var clone=$('#master_div').clone().find("label", "#add_input").hide().end().appendTo('#slave_div');
-                  
-  
-                  });
+            </div>
+{{-- end of card  body--}}
 
-                  $(document).on('click', '.btn_remove', function () {
-                var button_id = $(this).attr("id");
-                $('#slave_div').remove();
-            });
-        
+
+
+
+
+          </div>
+        {{-- end of card --}}
+
+        <script>
+          $(document).ready(function () {
+
+
+              $('#add_more').click(function (e) {
+
+
+                $('#master-div').clone().find("label").text("").end().appendTo('#slave-div');
+                <div class="d-flex  btn btn-link" id="remove-div" type="button" >
+
+</div>
               });
 
 
-           
+              $(document).on('click', '#remove', function () {
+                  var button_id = $(this).attr("id");
+                  $('#row' + button_id + '').remove();
+              });
 
-          </script>
+
+            //   <button class="btn-danger btn_remove" name="remove" id="' +i+ '"  type="button">
 
 
-    
+
+          });
+
+      </script>
+
 </x-app-layout>
 
- 
+
