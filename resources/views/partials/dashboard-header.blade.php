61606d752bfbdf8c3185287843ddaaf491ebfd95
Your Name <innovazania@gmail.com>
2024-02-15 07:13:51 +0200

    update

--- resources/views/partials/dashboard-header.blade.php
+++ resources/views/partials/dashboard-header.blade.php
@@ -82,13 +82,10 @@
             <div class="col-md-8">
                 <h4 class="font-20 weight-500 mb-10 text-capitalize">
                     Welcome back
-                    <div class="weight-600 font-30 text-blue">Johnny Brown!</div>
+                    <div class="weight-600 font-30 text-blue">Mnotfo Dlamini!</div>
                 </h4>
                 <p class="font-18 max-width-600">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde
-                    hic non repellendus debitis iure, doloremque assumenda. Autem
-                    modi, corrupti, nobis ea iure fugiat, veniam non quaerat
-                    mollitia animi error corporis.
+                  SHUNIFU X
                 </p>
             </div>
         </div>
